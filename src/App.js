06fa5043ca01874f19f2adba235060241cb3c52d
nickubed <nhpquandt@gmail.com>
2021-04-05 16:07:59 -0700

    Initial commit, jobs done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react'
 import './App.css';
+import ArtDisplay from './ArtDisplay'
+import ButtonBar from './ButtonBar'
 
 function App() {
+  let [data, setData] = useState({})
+  let [objectId, setObjectId] = useState(12755)
+  const errorImg = 'https://placekitten.com/200/200'
+
+  let [objectImg, setObjectImg] = useState(errorImg)
+
+
+  useEffect(() => {
+    document.title='Welcome to ArtWorld'
+    setObjectImg(null)
+    fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects/${objectId}`)
+    .then(response => response.json())
+    .then(resdata => {
+      setData(resdata)
+      setObjectImg(resdata.primaryImage)
+    })
+  }, [objectId])
+
+  const handleIterate = (e) => {
+    setObjectId(objectId + Number(e.target.value))
+  }
+
+  const displayImage = () => {
+    if(!objectImg) {
+      return (
+        <h2>Loading...</h2>
+      )
+    }
+    return (
+      <ArtDisplay objectImg={objectImg} title={data.title} />
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{data.title}</h1>
+      <div style={{'width': '100%'}}>
+        {displayImage()}
+      </div>
+      <ButtonBar handleIterate={handleIterate} />
     </div>
   );
 }
---
+++ src/ArtDisplay.js
@@ -0,0 +1,9 @@
+const ArtDisplay = (props) => {
+    return (
+        <div style={{'width': '100%'}}>
+            <img style={{'maxWidth': '100vw'}} src={props.objectImg} alt={props.title} />
+        </div>
+    )
+}
+
+export default ArtDisplay
